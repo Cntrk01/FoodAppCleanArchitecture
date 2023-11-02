57a55d12839e915ea76e2b6811049490a03a4076
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2023-11-02 17:39:39 +0300

    Detail Fragment
    Detay sayfasını yazdım çalışıyor.Webview da çalışıyor.Sadece favoriye ekleme durumunu yapmadım.

--- app/src/main/java/com/cantrk/foodappcleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/cantrk/foodappcleanarchitecture/MainActivity.kt
@@ -2,8 +2,11 @@ package com.cantrk.foodappcleanarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.cantrk.foodappcleanarchitecture.databinding.ActivityMainBinding
 import com.cantrk.foodappcleanarchitecture.ui.CategoryFragment
 import com.cantrk.foodappcleanarchitecture.ui.FavoriteFragment
@@ -12,9 +15,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val fragment1 = HomeFragment()
-    private val fragment2 = FavoriteFragment()
-    private val fragment3 = CategoryFragment()
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,19 +22,22 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.apply {
-            bottomNavigation.setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.homeFragment -> navController.navigate(R.id.homeFragment)
-                    R.id.favoriteFragment -> navController.navigate(R.id.favoriteFragment)
-                    R.id.categoryFragment -> navController.navigate(R.id.categoryFragment)
-                }
-                true
+        navHostFragment?.findNavController()?.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigation.visibility = when (destination.id) {
+                R.id.categoryFragment, R.id.homeFragment, R.id.favoriteFragment -> View.VISIBLE
+                else -> View.GONE
             }
+        }
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> navController.navigate(R.id.homeFragment)
+                R.id.favoriteFragment -> navController.navigate(R.id.favoriteFragment)
+                R.id.categoryFragment -> navController.navigate(R.id.categoryFragment)
+            }
+            true
         }
     }
 }
